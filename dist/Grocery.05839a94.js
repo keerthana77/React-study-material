93ed1a46d1b05eddf15e066def65748076b00ea8
Keerthana <kkeerthana77@gmail.com>
2023-11-28 16:18:27 +0800

    Add files via upload

---
+++ dist/Grocery.05839a94.js
@@ -0,0 +1,3 @@
+var e;(0,(e=("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequirefb24).register)("dhpq2",function(n,o){Object.defineProperty(n.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(n.exports,"default",{get:function(){return f},set:void 0,enumerable:!0,configurable:!0});var r=e("cc6u1");function f(){return/*#__PURE__*/(0,r.jsx)("h1",{children:"Our Grocery has many child components...."})}e("fIwXP")});//# sourceMappingURL=Grocery.05839a94.js.map
+
+//# sourceMappingURL=Grocery.05839a94.js.map
